80830d49db46ea548ddb4c83c37f8163b645d28b
anem1c <lshsean9123@gmail.com>
2025-01-24 03:29:25 +0900

    Update vite.config.js from feature/ui branch

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,18 @@ import { fileURLToPath, URL } from "url";
 const getAliasPath = (path) => {
   return fileURLToPath(new URL(path, import.meta.url));
 };
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      // node_modules 디렉토리와 프로젝트 루트 디렉토리에 대한 접근을 허용
+      allow: [
+        'node_modules/slick-carousel',
+        './'  // 프로젝트 루트 디렉토리 추가
+      ]
+    }
+  },
   resolve: {
     alias: {
       "@Components": getAliasPath("./src/Components"),
